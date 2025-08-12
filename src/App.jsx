ad4ffe6f7471c1610dadfc735b26f925207afc43
seyniss <shepunk2@kbu.ac.kr>
2025-08-12 14:09:10 +0900

    1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './components/Counter'
+
+function App() {
+
+
+  return (
+    <div>
+      <Counter />
+
+
+    </div>
+  )
+}
+
+export default App
